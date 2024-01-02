238a958eada13004bf4470ac2071b0a5c8bfccfe
Parakram <89983841+Parakramk@users.noreply.github.com>
2024-01-02 21:23:39 +0545

    Add http Handler

--- bin/vgyupload.dart
+++ bin/vgyupload.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 import 'dart:async';
 import 'package:path/path.dart' as p;
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 Future<String?> uploadImage(String filepath, String apiKey,
     {String url = 'https://vgy.me/upload'}) async {
@@ -23,6 +23,32 @@ Future<String?> uploadImage(String filepath, String apiKey,
   }
   final fileName = p.basename(filepath);
   final fileExtension = p.extension(fileName).toLowerCase();
+  if (!allowedExtensions.contains(fileExtension)) {
+    print('Error: $filepath has an invalid file extension');
+    return null;
+  }
+  var request = http.MultipartRequest('POST', Uri.parse(url));
+  request.files.add(await http.MultipartFile.fromPath('file', filepath));
+  request.fields['userkey'] = apiKey;
+  try {
+    var response = await request.send();
+    if (response.statusCode == 200) {
+      var resBody = await response.stream.bytesToString();
+      var data = json.decode(resBody);
+      if (data['error'] != null) {
+        print('Error: ${data['message']}');
+        return null;
+      }
+      String imageUrl = data['image'];
+      return imageUrl;
+    } else {
+      print('Error: HTTP ${response.statusCode} -- ${response.reasonPhrase}');
+      return null;
+    }
+  } catch (e) {
+    print('Error: $e');
+    return null;
+  }
 }
 
 void main(List<String> arguments) {
